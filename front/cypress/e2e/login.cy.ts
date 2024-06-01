451075eded2efb56609337f297734deef9f0f897
TBERT31 <berteau.thomas@hotmail.fr>
2024-06-01 21:05:38 +0200

    End of Jest testing

--- front/cypress/e2e/login.cy.ts
+++ front/cypress/e2e/login.cy.ts
@@ -1,3 +1,5 @@
+/// <reference types="cypress" />
+
 describe('Login spec', () => {
   it('Login successfull', () => {
     cy.visit('/login')
--- front/src/app/features/sessions/components/detail/detail.component.spec.ts
+++ front/src/app/features/sessions/components/detail/detail.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { ActivatedRoute, Router } from '@angular/router';
 import { expect } from '@jest/globals';
 import { By } from '@angular/platform-browser';
@@ -12,7 +12,7 @@ import { Session } from '../../interfaces/session.interface';
 import { Teacher } from '../../../../interfaces/teacher.interface';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
-describe('DetailComponent', () => {
+describe('DetailComponent user already participate to session', () => {
   let component: DetailComponent;
   let fixture: ComponentFixture<DetailComponent>;
   let sessionApiService: SessionApiService;
@@ -27,14 +27,29 @@ describe('DetailComponent', () => {
     };
 
     const mockSessionApiService = {
-      detail: jest.fn(),
+      detail: jest.fn().mockReturnValue(of({
+        id: 1,
+        name: 'Test Session',
+        description: 'Test Description',
+        date: new Date(),
+        teacher_id: 1,
+        users: [1,2,3],
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      })),
       delete: jest.fn().mockReturnValue(of(null)),
       participate: jest.fn(),
       unParticipate: jest.fn()
     };
 
     const mockTeacherService = {
-      detail: jest.fn()
+      detail: jest.fn().mockReturnValue(of({
+        id: 1,
+        lastName: 'Doe',
+        firstName: 'John',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }))
     };
 
     const mockRouter = {
@@ -87,7 +102,7 @@ describe('DetailComponent', () => {
       description: 'Test Description',
       date: new Date(),
       teacher_id: 1,
-      users: [1, 2, 3],
+      users: [1,2,3],
       createdAt: new Date(),
       updatedAt: new Date(),
     };
@@ -139,54 +154,139 @@ describe('DetailComponent', () => {
     expect(sessionApiService.unParticipate).toHaveBeenCalledWith('1', '1');
   });
 
-  // it('should go back when back button is clicked', () => {
-  //   const historySpy = jest.spyOn(window.history, 'back');
+  it('should go back when back button is clicked', () => {
+    fixture.detectChanges();
   
-  //   const backButton = fixture.debugElement.query(By.css('.back-button'));
+    const backButton = fixture.debugElement.query(By.css('.back-button'));
+    expect(backButton).toBeTruthy();
+  
 
-  //   backButton.triggerEventHandler('click', null);
-  //   expect(historySpy).toHaveBeenCalled();
+    const historySpy = jest.spyOn(window.history, 'back');
+    backButton.triggerEventHandler('click', null);
+  
+    expect(historySpy).toHaveBeenCalled();
+  });
 
-  // });
+  it('should show delete button only if user is admin', () => {
+    component.isAdmin = true;
+    component.sessionId = '1';
+    fixture.detectChanges();
+    const deleteButton = fixture.debugElement.query(By.css('.delete-button'));
+    expect(deleteButton).toBeTruthy();
+  });
 
-  // it('should show delete button only if user is admin', () => {
-  //   component.isAdmin = true;
-  //   component.sessionId = '1';
-  //   fixture.detectChanges();
-  //   const deleteButton = fixture.debugElement.query(By.css('.delete-button'));
-  //   expect(deleteButton).toBeTruthy();
-  // });
-
-  // it('should show participate button only if user is not admin and isParticipate is false', () => {
+  // it('should show participate button only if user is not admin and user do not participate', async () => {
   //   component.isAdmin = false;
   //   component.isParticipate = false;
   //   fixture.detectChanges();
-  //   const participateButton = fixture.debugElement.query(By.css('.participate-button'));
+  //   const participateButton = fixture.debugElement.nativeElement.querySelector('.participate-button');
   //   expect(participateButton).toBeTruthy();
   // });
 
-  // it('should show unparticipate button only if user is not admin and isParticipate is true', () => {
-  //   component.isAdmin = false;
-  //   component.isParticipate = true;
-  //   fixture.detectChanges();
-  //   const unparticipateButton = fixture.debugElement.query(By.css('.unparticipate-button'));
-  //   expect(unparticipateButton).toBeTruthy();
-  // });
+  it('should show unparticipate button only if user is not admin and user already participate', () => {
+    component.isAdmin = false;
+    component.isParticipate = true;
+    fixture.detectChanges();
+    const unparticipateButton = fixture.debugElement.query(By.css('.unparticipate-button'));
+    expect(unparticipateButton).toBeTruthy();
+  });
 
-  // it('should display teacher information correctly', () => {
-  //   const teacher: Teacher = { id: 1, lastName: 'Doe', firstName: 'John', createdAt: new Date(), updatedAt: new Date() };
-  //   component.teacher = teacher;
-  //   fixture.detectChanges();
-  //   const teacherName = fixture.debugElement.query(By.css('.teacher-name'));
-  //   expect(teacherName.nativeElement.textContent).toContain(`${teacher.firstName} ${teacher.lastName}`);
-  // });
+  it('should display teacher information correctly', () => {
+    const teacher: Teacher = { id: 1, lastName: 'Doe', firstName: 'John', createdAt: new Date(), updatedAt: new Date() };
+    component.teacher = teacher;
+    fixture.detectChanges();
+    const teacherName = fixture.debugElement.query(By.css('.teacher-name'));
+    expect(teacherName.nativeElement.textContent).toContain(`${teacher.firstName} ${teacher.lastName.toUpperCase()}`);
+  });
 
-  // it('should display session information correctly', () => {
-  //   const session: Session = { id: 1, name: 'Test Session', description: 'Test Description', date: new Date(), teacher_id: 1, users: [1], createdAt: new Date(), updatedAt: new Date() };
-  //   component.session = session;
-  //   fixture.detectChanges();
-  //   const sessionName = fixture.debugElement.query(By.css('.session-name'));
-  //   expect(sessionName.nativeElement.textContent).toContain(session.name);
-  // });
+  it('should display session information correctly', () => {
+    const session: Session = { id: 1, name: 'Test Session', description: 'Test Description', date: new Date(), teacher_id: 1, users: [1], createdAt: new Date(), updatedAt: new Date() };
+    component.session = session;
+    fixture.detectChanges();
+    const sessionName = fixture.debugElement.query(By.css('.session-name'));
+    expect(sessionName.nativeElement.textContent).toContain(session.name);
+  });
+  
+});
+
+describe('DetailComponent user do not participate to session yet', () => {
+  let component: DetailComponent;
+  let fixture: ComponentFixture<DetailComponent>;
+  let sessionApiService: SessionApiService;
+  let teacherService: TeacherService;
+  let sessionService: SessionService;
+  let matSnackBar: MatSnackBar;
+
+  beforeEach(async () => {
+    const mockSessionService = {
+      sessionInformation: { admin: false, id: 1 }
+    };
+
+    const mockSessionApiService = {
+      detail: jest.fn().mockReturnValue(of({
+        id: 1,
+        name: 'Test Session',
+        description: 'Test Description',
+        date: new Date(),
+        teacher_id: 1,
+        users: [],
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      })),
+      delete: jest.fn().mockReturnValue(of(null)),
+      participate: jest.fn(),
+      unParticipate: jest.fn()
+    };
+
+    const mockTeacherService = {
+      detail: jest.fn().mockReturnValue(of({
+        id: 1,
+        lastName: 'Doe',
+        firstName: 'John',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }))
+    };
+
+    const mockMatSnackBar = {
+      open: jest.fn()
+    };
+
+    await TestBed.configureTestingModule({
+      declarations: [DetailComponent],
+      providers: [
+        FormBuilder,
+        { provide: SessionApiService, useValue: mockSessionApiService },
+        { provide: TeacherService, useValue: mockTeacherService },
+        { provide: SessionService, useValue: mockSessionService },
+        { provide: MatSnackBar, useValue: mockMatSnackBar },
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              paramMap: {
+                get: () => '1', 
+              },
+            },
+          },
+        },
+      ],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(DetailComponent);
+    component = fixture.componentInstance;
+    sessionApiService = TestBed.inject(SessionApiService);
+    teacherService = TestBed.inject(TeacherService);
+    sessionService = TestBed.inject(SessionService);
+    matSnackBar = TestBed.inject(MatSnackBar);
+  });
+
+  it('should show participate button only if user is not admin and user do not participate', async () => {
+    component.isAdmin = false;
+    component.isParticipate = false;
+    fixture.detectChanges();
+    const participateButton = fixture.debugElement.nativeElement.querySelector('.participate-button');
+    expect(participateButton).toBeTruthy();
+  });
   
 });
