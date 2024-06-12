b432d7d8afc648a632cd021e66c29d4bfb3526a0
TBERT31 <berteau.thomas@hotmail.fr>
2024-06-12 21:32:49 +0200

    Factorize list.component.spec.ts in one test suit

--- front/src/app/features/sessions/components/list/list.component.spec.ts
+++ front/src/app/features/sessions/components/list/list.component.spec.ts
@@ -11,24 +11,21 @@ import { ListComponent } from './list.component';
 import { DatePipe } from '@angular/common';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { ReactiveFormsModule } from '@angular/forms';
+import { SessionInformation } from 'src/app/interfaces/sessionInformation.interface';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+
+
 
 const mockSessions = [
   { id: 1, name: 'Session 1', description: 'Description 1', date: new Date(), teacher_id: 1, users: [1,2] },
   { id: 2, name: 'Session 2', description: 'Description 2', date: new Date(), teacher_id: 2, users: [3,4,5] }
 ];
 
-describe('ListComponent for Admin', () => {
+describe('ListComponent', () => {
   let component: ListComponent;
   let fixture: ComponentFixture<ListComponent>;
   let sessionApiService: SessionApiService;
-  let datePipe: DatePipe;
-
-  const mockAdminSessionService = {
-    sessionInformation: {
-      admin: true
-    }
-  }
+  let sessionService: SessionService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -41,17 +38,31 @@ describe('ListComponent for Admin', () => {
         ReactiveFormsModule
       ],
       providers: [
-        { provide: SessionService, useValue: mockAdminSessionService },
-        { provide: SessionApiService, useValue: { all: jest.fn().mockReturnValue(of(mockSessions)) } }
+        { 
+          provide: SessionApiService, useValue: { all: jest.fn().mockReturnValue(of(mockSessions)) }
+        }
       ],
       schemas: [NO_ERRORS_SCHEMA]
     })
-      .compileComponents();
+    .compileComponents();
 
     fixture = TestBed.createComponent(ListComponent);
-    datePipe = new DatePipe('en-US'); 
     component = fixture.componentInstance;
     sessionApiService = TestBed.inject(SessionApiService);
+    sessionService = TestBed.inject(SessionService);
+
+    Object.defineProperty(sessionService, 'sessionInformation', {
+      get: jest.fn().mockReturnValue({
+        token: 'some-token',
+        type: 'user',
+        id: 2,
+        username: 'regularUser',
+        firstName: 'Regular',
+        lastName: 'User',
+        admin: false
+      })
+    });
+
     fixture.detectChanges();
   });
 
@@ -63,22 +74,52 @@ describe('ListComponent for Admin', () => {
     expect(sessionApiService.all).toHaveBeenCalled();
   });
 
-  it('should show Create button', () => {
+
+  it('should display the Create button when user is admin',  () => {
+    jest.spyOn(sessionService, 'sessionInformation', 'get').mockReturnValue({
+      token: 'some-token',
+      type: 'admin',
+      id: 1,
+      username: 'adminUser',
+      firstName: 'Admin',
+      lastName: 'User',
+      admin: true
+    });
+
+    fixture.detectChanges();
     const button = fixture.debugElement.query(By.css('button[routerLink="create"]'));
     expect(button).not.toBeNull();
   });
 
-  it('should show Edit button for each session', async () => {
-    await fixture.whenStable();
+  it('should not display the Create button when user is not admin', () => {
+    const button = fixture.debugElement.query(By.css('button[routerLink="create"]'));
+    expect(button).toBeNull();
+  });
+
+  it('should display Edit buttons for each session when user is admin', () => {
+    jest.spyOn(sessionService, 'sessionInformation', 'get').mockReturnValue({
+      token: 'some-token',
+      type: 'admin',
+      id: 1,
+      username: 'adminUser',
+      firstName: 'Admin',
+      lastName: 'User',
+      admin: true
+    });
+
     fixture.detectChanges();
     const buttons = fixture.debugElement.queryAll(By.css('.edit-button'));
     expect(buttons.length).toBe(mockSessions.length);
   });
 
-  it('should display correct session information', async () => {
-    await fixture.whenStable();
-    fixture.detectChanges();
-  
+  it('should not display Edit buttons for any session when user is not admin', () => {
+    const buttons = fixture.debugElement.queryAll(By.css('.edit-button'));
+    expect(buttons.length).toBe(0);
+  });
+
+
+  it('should display correct session information', () => {
+    const datePipe = new DatePipe('en-US');
     const cardTitles = fixture.debugElement.queryAll(By.css('.session-name'));
     const cardSubtitles = fixture.debugElement.queryAll(By.css('.session-date'));
     const cardContents = fixture.debugElement.queryAll(By.css('.session-description'));
@@ -89,50 +130,5 @@ describe('ListComponent for Admin', () => {
       expect(cardContents[i].nativeElement.textContent).toContain(mockSessions[i].description);
     }
   });
-});
-
-describe('ListComponent for User', () => {
-  let component: ListComponent;
-  let fixture: ComponentFixture<ListComponent>;
-  let sessionApiService: SessionApiService;
-
-  const mockUserSessionService = {
-    sessionInformation: {
-      admin: false
-    }
-  }
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ListComponent],
-      imports: [HttpClientTestingModule, MatCardModule, MatIconModule],
-      providers: [
-        { provide: SessionService, useValue: mockUserSessionService },
-        { provide: SessionApiService, useValue: { all: jest.fn().mockReturnValue(of(mockSessions)) } }
-      ],
-      schemas: [NO_ERRORS_SCHEMA]
-    })
-      .compileComponents();
-
-    fixture = TestBed.createComponent(ListComponent);
-    component = fixture.componentInstance;
-    sessionApiService = TestBed.inject(SessionApiService);
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-
-  it('should not show Create button', () => {
-    const button = fixture.debugElement.query(By.css('button[routerLink="create"]'));
-    expect(button).toBeNull();
-  });
-
-  it('should not show Edit button for each session', async () => {
-    await fixture.whenStable();
-    fixture.detectChanges();
-    const buttons = fixture.debugElement.queryAll(By.css('.edit-button'));
-    expect(buttons.length).toBe(0);
-  });
+  
 });
